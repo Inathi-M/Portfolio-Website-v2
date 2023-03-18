42a0b8630923527ffd3773867535349d65b0ff2f
Nkosinathi Tshaphile <tshaphilenkosinathi@gmail.com>
2023-03-18 16:01:22 +0200

    Completed the first page.

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+//import {BsLinkedin} from 'react-icons/bs'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://linkedin.com" target="_blank">
+        Linkedin
+      </a>
+      <a href="https://github.com" target="_blank">
+        GitHub
+      </a>
+      <a href="https://leetcode .com" target="_blank">
+        Leetcode
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
