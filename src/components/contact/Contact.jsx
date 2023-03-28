c7a3a355aab6f796969de0e67b709893b6a2f22d
Nkosinathi Tshaphile <tshaphilenkosinathi@gmail.com>
2023-03-28 21:29:51 +0200

    Working on the Contact.css component. Adding styling.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./contact.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
   return (
@@ -11,27 +12,53 @@ const Contact = () => {
 
       <div className="container contact__container">
         <div className="contact__options">
-          <article className="contact__options">
-            <MdOutlineEmail />
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>tshaphilenkosinathi@gmail.com</h5>
-            <a href="mailto:tshaphilenkosinathi@gmail.com">Send me an E-mail</a>
+            <a href="mailto:tshaphilenkosinathi@gmail.com" target="_blank">
+              Send me an E-mail
+            </a>
           </article>
-          <article className="contact__options">
-            <MdOutlineEmail />
-            <h4>Email</h4>
-            <h5>tshaphilenkosinathi@gmail.com</h5>
-            <a href="mailto:tshaphilenkosinathi@gmail.com">Send me an E-mail</a>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+27 62 033 8048</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+27620338048"
+              target="_blank"
+            >
+              Send me a message
+            </a>
           </article>
-          <article className="contact__options">
-            <MdOutlineEmail />
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
             <h4>Messenger</h4>
             <h5>Nkosinathi Tshaphile</h5>
-            <a href="https://m.me/nkosinathi.tshaphile">Send me a DM</a>
+            <a href="https://m.me/nkosinathi.tshaphile" target="_blank">
+              Send me a DM
+            </a>
           </article>
         </div>
         {/**END OF CONTACT OPTIONS */}
-        <form action=""></form>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
