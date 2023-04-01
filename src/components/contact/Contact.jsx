978e337243d81e421acfcce8ed3b978e3d5e3f4c
Nkosinathi Tshaphile <tshaphilenkosinathi@gmail.com>
2023-04-01 15:41:50 +0200

    Done with Footer Components. Needs to install emailjs and configure it.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,32 @@ import "./contact.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { BsWhatsapp } from "react-icons/bs";
 import { RiMessengerLine } from "react-icons/ri";
+import { useRef } from "react";
+//import emailjs from "emailjs-com";
 
 const Contact = () => {
+  /*const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_USER_ID"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };*/
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
